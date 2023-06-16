04914035c6a2e75fd6e92e367ad7fcc81eedaeb4
theprocess89 <josephnzehai@gmail.com>
2023-06-15 18:08:05 -0700

    c-hello,World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * main : Entry point
+ * main - Entry point
+ *
  * return : always 0 (sucess)
  */
 int main(void) 
-{ puts("\"Programming is like building a multilingual puzzle");
+{ 
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
