43127f03984eb05cb281db9d477940a84a1f313a
theprocess89 <josephnzehai@gmail.com>
2023-06-20 08:14:08 -0700

    C - Functions, nested loops

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes here */
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is positive\n", n);
+	printf("%d is positive\n", n);
 }
-if (n < 0)
+else if (n == 0)
 {
-printf("%d is negative\n", n);
+	printf("%d is zero\n", n);
 }
-if (n == 0)
+else
 {
-printf("%d is zero\n", n);
+	printf("%d is negative\n", n);
 }
 return (0);
 }
-
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'Prints _putchar to stdout'
+ * Return: 0
+ */
+int main(void)
+{
+	_puchar('_');
+	_puchar('p');
+	_puchar('u');
+	_puchar('t');
+	_puchar('c');
+	_puchar('h');
+	_puchar('a');
+	_puchar('a');
+	_puchar('r');
+	_puchar('\n');
+	return (0);
+}
