e07508a34017b95133fbd2422f8c71973f00b804
theprocess89 <josephnzehai@gmail.com>
2023-06-22 00:52:43 -0700

    C - Debugging

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * main - Entry point
- * Description: 'Prints _putchar to stdout'
+ * main - print _putchar
+ * is written by mbah
  * Return: 0
  */
+
 int main(void)
 {
-	_puchar('_');
-	_puchar('p');
-	_puchar('u');
-	_puchar('t');
-	_puchar('c');
-	_puchar('h');
-	_puchar('a');
-	_puchar('a');
-	_puchar('r');
-	_puchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
